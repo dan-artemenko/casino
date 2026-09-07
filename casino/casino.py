ec34ee39b77195828a0c0f66d14b5adc71c16339
dan-artemenko <alderartal@yandex.ru>
2026-09-07 14:04:23 +0300

    перевел в ООП

--- casino/casino.py
+++ casino/casino.py
@@ -1,23 +1,32 @@
 import random
+class casinorandom:
+    def __init__(self):
+        self.symbols = ["🍒", "🍋", "💎", "⭐"]
+        self.name = ""
+        self.coin = 100
+    def privet(self):
+        """приветствие"""
+        print("Добро пожаловать в казино")
+        print("Как тебя зовут?")
+        self.name = input()
+        print(f"Рад знакомству, {self.name}")
+        print(f'Ваш баланс {self.coin}')
+    def baraban(self):
+        """логика барабана"""
+        user = []
+        for i in range(0,3):
+            drum = random.choice(self.symbols)
+            user.append(drum)
 
+        print(f"🎰 На барабане выпало: {user}")
+    def start(self):
+        """запуск"""
+        self.privet()
 
-print("Добро пожаловать в казино")
-print("Как тебя зовут?")
-name = input()
-print(f"Рад знакомству, {name}")
-coin = 100
-print(f'Ваш баланс {coin}')
-print(f"готов делать ставки, {name}?")
-a = input()
-symbols = ["🍒", "🍋", "💎", "⭐"]
-def casino_game():
-    user = []
-    for i in range(0,3):
-        drum = random.choice(symbols)
-        user.append(drum)
+        print(f"готов делать ставки, {self.name}?")
+        a = input()
 
-
-    print(f"🎰 На барабане выпало: {user}")
-if a == "да":
-    casino_game()
-    
+        if a == "да":
+            self.baraban()
+game = casinorandom()
+game.start()
